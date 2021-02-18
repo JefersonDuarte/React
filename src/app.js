2c2b8532ed4883b70dc22a072b8a7afbb4e6b5ac
Jeferson Duarte de Oliveira <jeferson_duartemk@outlook.com>
2021-02-18 13:28:38 -0300

    feat(general): implements props

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const App = React.createClass({
   render: function () {
     return (
     <div className="container">
-      <Title name="Jeferson Duarte"/>
+      <Title name="Jeferson Duarte" lastname={5} />
       <label htmlFor="input">Input</label>
       <input type="text" name="input"></input>
     </div>
--- src/title.js
+++ src/title.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Title = React.createClass({
   render: function () {
     return (
-      <h1>Olá {this.props.name}</h1>
+      <h1>Olá {this.props.name + ' ' + this.props.lastname}</h1>
     )
   }
 })
