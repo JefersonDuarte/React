0f57cf419f7e1708c629816882fb71159200fb36
Jeferson Duarte de Oliveira <jeferson_duartemk@outlook.com>
2021-02-18 13:10:10 -0300

    feat(general): props html default

--- src/app.js
+++ src/app.js
@@ -6,8 +6,10 @@ import Title from "./title"
 const App = React.createClass({
   render: function () {
     return (
-    <div>
+    <div className="container">
       <Title name="Jeferson Duarte"/>
+      <label htmlFor="input">Input</label>
+      <input type="text" name="input"></input>
     </div>
     )
   },
