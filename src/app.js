c5c0853f14efd7fffc9927e6d38d52474e99bcef
Jeferson Duarte de Oliveira <jeferson_duartemk@outlook.com>
2021-02-19 13:29:25 -0300

    feat(app): create cmponent using class

--- src/app.js
+++ src/app.js
@@ -1,18 +1,31 @@
 "use-strict";
 
-import React from "react"
-import Title from "./title"
+import React, {Component} from "react";
+import Title from "./title";
 
-const App = React.createClass({
-  render: function () {
+class App extends Component {
+  render() {
     return (
-    <div className="container">
-      <Title name="Jeferson Duarte" lastname={5} />
-      <label htmlFor="input">Input</label>
-      <input type="text" name="input"></input>
-    </div>
-    )
-  },
-});
+      <div className="container">
+        <Title name="Jeferson Duarte" lastname={5} />
+        <label htmlFor="input">Input</label>
+        <input type="text" name="input"></input>
+      </div>
+    );
+  }
+}
 
-export default App
+
+// const App = React.createClass({
+//   render: function () {
+//     return (
+//     <div className="container">
+//       <Title name="Jeferson Duarte" lastname={5} />
+//       <label htmlFor="input">Input</label>
+//       <input type="text" name="input"></input>
+//     </div>
+//     )
+//   },
+// });
+
+export default App;
