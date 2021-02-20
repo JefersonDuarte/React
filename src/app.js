71cbf3a245dfd7a6882e37098720059a23429899
Jeferson Duarte de Oliveira <jeferson_duartemk@outlook.com>
2021-02-20 15:19:39 -0300

    feat(app): understand key property

--- src/app.js
+++ src/app.js
@@ -2,14 +2,17 @@
 
 import React, {Component} from "react";
 import Title from "./title";
+import Square from "./square"
+
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-        <Title name="Jeferson Duarte" lastname={5} />
-        <label htmlFor="input">Input</label>
-        <input type="text" name="input"></input>
+        {["#ccc", "#f00", "#0f0"].map((square) =>(
+          <Square key={square} color={square} />
+        ))}
+
       </div>
     );
   }
