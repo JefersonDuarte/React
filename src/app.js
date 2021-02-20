0698a52c9bc854942c80ae288766b677eb92aac9
Jeferson Duarte de Oliveira <jeferson_duartemk@outlook.com>
2021-02-20 16:45:39 -0300

    feat(app): working with events

--- src/app.js
+++ src/app.js
@@ -1,23 +1,20 @@
 "use-strict";
 
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Title from "./title";
-import Square from "./square"
-
+import Square from "./square";
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        {["#ccc", "#f00", "#0f0"].map((square) =>(
-          <Square key={square} color={square} />
-        ))}
-
+      <div className="container" onClick={(e) => {
+        alert('Cliclou')
+      }}>
+        <Square color="red"/>
       </div>
     );
   }
 }
-
 
 // const App = React.createClass({
 //   render: function () {
